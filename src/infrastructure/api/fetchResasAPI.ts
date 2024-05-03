edfc720c0eca6949652b8861d6a9aa0c8c50a2bc
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-05-04 03:23:06 +0900

    feat: RESAS APIのAPIキーをURLパラメータから渡せるように変更

--- src/infrastructure/api/fetchResasAPI.ts
+++ src/infrastructure/api/fetchResasAPI.ts
@@ -1,9 +1,13 @@
+import { getURLParams } from "../../helpers/getURLParams";
+
 export const fetchResasAPI = async (
   endpoint: string,
   method: string = "GET",
   body?: any, // eslint-disable-line @typescript-eslint/no-explicit-any
 ) => {
-  const API_KEY = import.meta.env.VITE_RESAS_API_KEY;
+  const API_KEY =
+    import.meta.env.VITE_RESAS_API_KEY ||
+    getURLParams(window.location.href).resas_api_key;
   if (!API_KEY) {
     throw new Error("RESAS_API_KEY is not defined");
   }
