27157358875f065d79891de6db69198c8feba6be
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-05-10 07:10:33 +0900

    feat: Appコンポーネントのスタイルを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useCallback, useEffect, useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
 import PopulationCategoryRadioButtonGroup from "./components/organisms/PopulationCategoryRadioButtonGroup";
 import {
   PopulationChart,
@@ -9,11 +7,42 @@ import {
 import { PrefectureCheckBoxGroup } from "./components/organisms/PrefectureCheckBoxGroup";
 import { PopulationCompositionPerYearValue } from "./core/domain/models/Population";
 import { PopulationController } from "./core/interface/PopulationController";
-import viteLogo from "/vite.svg";
 
 import { PopulationCategory } from "./core/domain/models/Population";
 import { Prefecture } from "./core/domain/models/Prefecture";
 import { PrefectureController } from "./core/interface/PrefectureController";
+
+import { Reset } from "styled-reset";
+
+import styled from "styled-components";
+import { ErrorAlert } from "./components/atoms/ErrorAlert";
+import Title from "./components/atoms/Title";
+
+const Root = styled.div`
+  min-height: 100vh;
+  background: #f7f7fd;
+`;
+
+const Container = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 1rem;
+  text-align: center;
+`;
+
+const Section = styled.section`
+  padding: 1em;
+  margin: 1em;
+  background: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const ChartSection = styled(Section)`
+  & > * {
+    max-height: 500px;
+  }
+`;
 
 interface CategoryPopulationPlotPoints {
   totalPopulationPlotPoints: PopulationPlotPoint[];
@@ -23,7 +52,6 @@ interface CategoryPopulationPlotPoints {
 }
 
 const App = () => {
-  const [count, setCount] = useState(0);
   const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
   const [errorMessage, setErrorMessage] = useState<string | undefined>(
     undefined,
@@ -60,131 +88,120 @@ const App = () => {
   }, [pendingSelectedCategory]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      {errorMessage && (
-        <section>
-          <div>
-            <p>都道府県一覧の取得に失敗しました。</p>
-            <p>{errorMessage}</p>
-          </div>
-        </section>
-      )}
-      <section>
-        <PrefectureCheckBoxGroup
-          prefectures={prefectures}
-          onSelectedPrefecturesChange={useCallback(async (prefectures) => {
-            const populationController = new PopulationController();
-            const populationCompositionPerYearList: {
-              prefName: string;
-              totalPopulation: PopulationCompositionPerYearValue[];
-              youngPopulation: PopulationCompositionPerYearValue[];
-              productiveAgePopulation: PopulationCompositionPerYearValue[];
-              elderlyPopulation: PopulationCompositionPerYearValue[];
-            }[] = [];
-
-            for (const prefecture of prefectures) {
-              const population =
-                await populationController.getPopulationCompositionPerYear(
-                  prefecture.prefCode,
-                );
-              populationCompositionPerYearList.push({
-                prefName: prefecture.prefName,
-                totalPopulation: population.totalPopulation,
-                youngPopulation: population.youngPopulation,
-                productiveAgePopulation: population.productiveAgePopulation,
-                elderlyPopulation: population.elderlyPopulation,
-              });
-            }
-
-            if (populationCompositionPerYearList.length === 0) {
-              return;
-            }
-
-            const totalPopulationPlotPoints: PopulationPlotPoint[] = [];
-            const youngPopulationPlotPoints: PopulationPlotPoint[] = [];
-            const productiveAgePopulationPlotPoints: PopulationPlotPoint[] = [];
-            const elderlyPopulationPlotPoints: PopulationPlotPoint[] = [];
-
-            for (const index of Array(
-              populationCompositionPerYearList[0].totalPopulation.length,
-            ).keys()) {
-              const totalPopulationPlotPoint: PopulationPlotPoint = {
-                year: populationCompositionPerYearList[0].totalPopulation[index]
-                  .year,
-              };
-              const youngPopulationPlotPoint = { ...totalPopulationPlotPoint };
-              const productiveAgePopulationPlotPoint = {
-                ...totalPopulationPlotPoint,
-              };
-              const elderlyPopulationPlotPoint = {
-                ...totalPopulationPlotPoint,
-              };
-              for (const {
-                prefName,
-                totalPopulation,
-                youngPopulation,
-                productiveAgePopulation,
-                elderlyPopulation,
-              } of populationCompositionPerYearList) {
-                totalPopulationPlotPoint[prefName] =
-                  totalPopulation[index].value;
-                youngPopulationPlotPoint[prefName] =
-                  youngPopulation[index].value;
-                productiveAgePopulationPlotPoint[prefName] =
-                  productiveAgePopulation[index].value;
-                elderlyPopulationPlotPoint[prefName] =
-                  elderlyPopulation[index].value;
-              }
-              totalPopulationPlotPoints.push(totalPopulationPlotPoint);
-              youngPopulationPlotPoints.push(youngPopulationPlotPoint);
-              productiveAgePopulationPlotPoints.push(
-                productiveAgePopulationPlotPoint,
-              );
-              elderlyPopulationPlotPoints.push(elderlyPopulationPlotPoint);
-            }
-            setPopulationPlotPoints({
-              totalPopulationPlotPoints,
-              youngPopulationPlotPoints,
-              productiveAgePopulationPlotPoints,
-              elderlyPopulationPlotPoints,
-            });
-          }, [])}
-        />
-      </section>
-      <section>
-        <PopulationCategoryRadioButtonGroup
-          onSelectedCategoryChange={setPendingSelectedCategory}
-        />
-      </section>
-      <section>
-        {populationPlotPoints && (
-          <PopulationChart
-            selectedCategory={selectedCategory}
-            {...populationPlotPoints}
-          />
+    <Root>
+      <Reset />
+      <Container>
+        <Title text="prefecture-population-visualizer" level="h1" />
+        {errorMessage && (
+          <section>
+            <ErrorAlert title="Error">
+              <p>都道府県一覧の取得に失敗しました。</p>
+              <p>{errorMessage}</p>
+            </ErrorAlert>
+          </section>
         )}
-      </section>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <Section>
+          <PrefectureCheckBoxGroup
+            prefectures={prefectures}
+            onSelectedPrefecturesChange={useCallback(async (prefectures) => {
+              const populationController = new PopulationController();
+              const populationCompositionPerYearList: {
+                prefName: string;
+                totalPopulation: PopulationCompositionPerYearValue[];
+                youngPopulation: PopulationCompositionPerYearValue[];
+                productiveAgePopulation: PopulationCompositionPerYearValue[];
+                elderlyPopulation: PopulationCompositionPerYearValue[];
+              }[] = [];
+
+              for (const prefecture of prefectures) {
+                const population =
+                  await populationController.getPopulationCompositionPerYear(
+                    prefecture.prefCode,
+                  );
+                populationCompositionPerYearList.push({
+                  prefName: prefecture.prefName,
+                  totalPopulation: population.totalPopulation,
+                  youngPopulation: population.youngPopulation,
+                  productiveAgePopulation: population.productiveAgePopulation,
+                  elderlyPopulation: population.elderlyPopulation,
+                });
+              }
+
+              if (populationCompositionPerYearList.length === 0) {
+                return;
+              }
+
+              const totalPopulationPlotPoints: PopulationPlotPoint[] = [];
+              const youngPopulationPlotPoints: PopulationPlotPoint[] = [];
+              const productiveAgePopulationPlotPoints: PopulationPlotPoint[] =
+                [];
+              const elderlyPopulationPlotPoints: PopulationPlotPoint[] = [];
+
+              for (const index of Array(
+                populationCompositionPerYearList[0].totalPopulation.length,
+              ).keys()) {
+                const totalPopulationPlotPoint: PopulationPlotPoint = {
+                  year: populationCompositionPerYearList[0].totalPopulation[
+                    index
+                  ].year,
+                };
+                const youngPopulationPlotPoint = {
+                  ...totalPopulationPlotPoint,
+                };
+                const productiveAgePopulationPlotPoint = {
+                  ...totalPopulationPlotPoint,
+                };
+                const elderlyPopulationPlotPoint = {
+                  ...totalPopulationPlotPoint,
+                };
+                for (const {
+                  prefName,
+                  totalPopulation,
+                  youngPopulation,
+                  productiveAgePopulation,
+                  elderlyPopulation,
+                } of populationCompositionPerYearList) {
+                  totalPopulationPlotPoint[prefName] =
+                    totalPopulation[index].value;
+                  youngPopulationPlotPoint[prefName] =
+                    youngPopulation[index].value;
+                  productiveAgePopulationPlotPoint[prefName] =
+                    productiveAgePopulation[index].value;
+                  elderlyPopulationPlotPoint[prefName] =
+                    elderlyPopulation[index].value;
+                }
+                totalPopulationPlotPoints.push(totalPopulationPlotPoint);
+                youngPopulationPlotPoints.push(youngPopulationPlotPoint);
+                productiveAgePopulationPlotPoints.push(
+                  productiveAgePopulationPlotPoint,
+                );
+                elderlyPopulationPlotPoints.push(elderlyPopulationPlotPoint);
+              }
+              setPopulationPlotPoints({
+                totalPopulationPlotPoints,
+                youngPopulationPlotPoints,
+                productiveAgePopulationPlotPoints,
+                elderlyPopulationPlotPoints,
+              });
+            }, [])}
+          />
+        </Section>
+        <Section>
+          <PopulationCategoryRadioButtonGroup
+            onSelectedCategoryChange={setPendingSelectedCategory}
+          />
+        </Section>
+
+        {populationPlotPoints && (
+          <ChartSection>
+            <PopulationChart
+              selectedCategory={selectedCategory}
+              {...populationPlotPoints}
+            />
+          </ChartSection>
+        )}
+      </Container>
+    </Root>
   );
 };
 
